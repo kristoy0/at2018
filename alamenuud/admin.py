421493ca895e213a552b9fab8a61fdc96e25e1c3
kristoy0 <kristoy0@hotmail.com>
2018-03-08 11:39:48 +0200

    change django title

--- alamenuud/admin.py
+++ alamenuud/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import Alamenuu, Sundmus, Uudis
 from django import forms
 
+admin.site.site_header = 'Administratsiooni paneel'
+
 # Register your models here.
+
 admin.site.register(Alamenuu)
 admin.site.register(Sundmus)
 admin.site.register(Uudis)
